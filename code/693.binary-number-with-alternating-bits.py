db26c423fc638e4ebe3aad1b7f253a48c1baf472
ProHiryu <songheqi1996@gmail.com>
2020-03-19 11:14:57 +0800

    update readme

---
+++ code/693.binary-number-with-alternating-bits.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=693 lang=python3
+#
+# [693] Binary Number with Alternating Bits
+#
+
+# @lc code=start
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        return '00' not in bin(n) and '11' not in bin(n)
+# @lc code=end
+
