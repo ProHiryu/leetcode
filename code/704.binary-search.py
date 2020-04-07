fd1c0919583f37402775b1f719e49e58a48416ae
ProHiryu <songheqi1996@gmail.com>
2020-04-07 15:26:15 +0800

    704

---
+++ code/704.binary-search.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m
+        return l if nums[l] == target else -1
+
+# @lc code=end
+
