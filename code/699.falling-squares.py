e855f05dafe93c32c657d1950e0971289afb42ee
ProHiryu <songheqi1996@gmail.com>
2020-04-08 09:51:29 +0800

    699

---
+++ code/699.falling-squares.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=699 lang=python3
+#
+# [699] Falling Squares
+#
+
+# @lc code=start
+class Solution:
+    def fallingSquares(self, positions: List[List[int]]) -> List[int]:
+        
+# @lc code=end
+
