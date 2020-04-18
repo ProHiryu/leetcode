64a12eacb44d167e06d550ea3fe3c18c79ae5b28
ProHiryu <songheqi1996@gmail.com>
2020-04-18 10:33:45 +0800

    code 709

---
+++ code/709.to-lower-case.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=709 lang=python3
+#
+# [709] To Lower Case
+#
+
+# @lc code=start
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return str.lower(s)
+# @lc code=end
+
